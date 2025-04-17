bd3e7861b72bd35f532e2b058c6b8adfeb88c820
namkungkang <namkung0131@naver.com>
2025-04-17 22:12:09 +0900

    [FEAT]#1 데이터베이스 세팅 수정

---
+++ src/main/java/com/parentsgowork/server/TestController.java
@@ -0,0 +1,15 @@
+package com.parentsgowork.server;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+    @GetMapping("/")
+    @ResponseBody
+    public String hello() {
+        return "hello";
+    }
+}
