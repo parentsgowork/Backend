129061bda30b5bd309fa9dee888d9ee63e146c08
namkungkang <namkung0131@naver.com>
2025-04-18 22:15:56 +0900

    feature/1/database-setting 수정

---
+++ src/main/java/com/parentsgowork/server/project/domain/User.java
@@ -0,0 +1,20 @@
+package com.parentsgowork.server.project.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
--- src/main/java/com/parentsgowork/server/project/web/controller/TestController.java
+++ src/main/java/com/parentsgowork/server/project/web/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.parentsgowork.server;
+package com.parentsgowork.server.project.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
