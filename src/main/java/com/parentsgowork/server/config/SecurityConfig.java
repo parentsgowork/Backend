619358342fb8782cf5a5bcc43ad9cba02741b552
벨라 <icomksh@gmail.com>
2025-05-04 21:49:49 +0900

    [#6] Refactor: 이슈 템플릿, 디렉토리 구조 변경

--- src/main/java/com/parentsgowork/server/config/SecurityConfig.java
+++ src/main/java/com/parentsgowork/server/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.parentsgowork.server;
+package com.parentsgowork.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- src/main/java/com/parentsgowork/server/web/controller/TestController.java
+++ src/main/java/com/parentsgowork/server/web/controller/TestController.java
@@ -1,4 +1,4 @@
-package com.parentsgowork.server.project.web.controller;
+package com.parentsgowork.server.web.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
