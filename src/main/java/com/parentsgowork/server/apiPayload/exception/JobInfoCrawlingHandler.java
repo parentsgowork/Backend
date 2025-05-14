479b0161df452ceef5275f0d2037731abb1d10f2
벨라 <icomksh@gmail.com>
2025-05-14 22:46:17 +0900

    Feat: 구직 정보 북마크 추가

--- src/main/java/com/parentsgowork/server/apiPayload/exception/JobInfoCrawlingHandler.java
+++
@@ -1,9 +0,0 @@
-package com.parentsgowork.server.apiPayload.exception;
-
-import com.parentsgowork.server.apiPayload.code.BaseErrorCode;
-
-public class JobInfoCrawlingHandler extends GeneralException {
-    public JobInfoCrawlingHandler(BaseErrorCode code) {
-        super(code);
-    }
-}
